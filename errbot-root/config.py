73b64b56f234ee1d79834cbb581492f82f6cd560
Reinier Evangelista <revangelista@securitycompass.com>
2019-08-17 21:09:38 -0400

    Add Slack integration

--- errbot-root/config.py
+++ errbot-root/config.py
@@ -5,7 +5,7 @@ import logging
 # the options in the more complete config-template.py from here:
 # https://raw.githubusercontent.com/errbotio/errbot/master/errbot/config-template.py
 
-BACKEND = 'Text'  # Errbot will start in text mode (console only mode) and will answer commands from there.
+BACKEND = 'Slack'  # Errbot will start in text mode (console only mode) and will answer commands from there.
 
 BOT_DATA_DIR = r'/Users/revangelista/dev/nosbot2/errbot-root/data'
 BOT_EXTRA_PLUGIN_DIR = r'/Users/revangelista/dev/nosbot2/errbot-root/plugins'
@@ -14,8 +14,8 @@ BOT_LOG_FILE = r'/Users/revangelista/dev/nosbot2/errbot-root/errbot.log'
 BOT_LOG_LEVEL = logging.DEBUG
 
 BOT_ADMINS = ('@r.evangel', )  # !! Don't leave that to "@CHANGE_ME" if you connect your errbot to a chat system !!
-BOT_ALT_PREFIXES = ('@nb2', )
+BOT_ALT_PREFIXES = ('@nostalgiabot', )
 
 BOT_IDENTITY = {
-    'token': 'xoxb-540473674164-631801720276-zLLzkINcvhpp7YmO2IAfnt6j',
+    'token': 'xoxb-540473674164-731953338614-MhSrPWgk9SOFWCJlZB9Oi49M',
 }
