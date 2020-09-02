d6e3560a7bd32e6f8872e91934c4edc1fd6a5eea
Kelly Bell <kbell@securitycompass.com>
2020-09-02 18:46:42 -0400

    Add quotes as an include filter to Person api
    refs #27

--- nb2/api.py
+++ nb2/api.py
@@ -9,22 +9,93 @@ bp = Blueprint("api", __name__)
 api = Api(bp)
 
 
-class PersonResourceBase(Resource):
+class IncludeFilterMixin:
+    """
+    A mixin for optionally including fields in an API response
+
+    To optionally include an API field, the relevant Resource class
+    should inherit from this mixin. The Resource class should then
+    implement a `get_<fieldname>_field_type` property method that
+    returns the type definition of the field to include.
+
+    Example:
+        To optionally include a string field named `foo` implement to following
+        on the appropriate Resource class
+
+        @property
+        def get_foo_field_type(self):
+            return fields.String
+
+        See flask-restful docs for defining field types
+        https://flask-restful.readthedocs.io/en/latest/fields.html#basic-usage
+
+    To include these the fields in the api response, add the query parameter
+    `include=fieldname`. Multiple fieldnames are allowed as well:
+    `include=fieldname,fieldname,fieldname`.
+
+    Note that this mixin will modify or create `self.field` and `self.parser`
+    properties on the class.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if not hasattr(self, 'fields'):
+            self.fields = {}
+
+        if not hasattr(self, 'parser'):
+            self.parser = reqparse.RequestParser(bundle_errors=True)
+
+        self.parser.add_argument('include', type=str, location='args')
+
+        self.include_fields()
+        super(IncludeFilterMixin, self).__init__(*args, **kwargs)
+
+    def include_fields(self):
+        parsed_args = self.parser.parse_args()
+        include_fields = parsed_args['include'] or []
+
+        if include_fields:
+            include_fields = include_fields.split(',')
+
+        for field in include_fields:
+            field_type = getattr(self, f"get_{field}_field_type", None)
+
+            if field_type:
+                self.fields[field] = field_type
+
+
+class PersonResourceBase(Resource, IncludeFilterMixin):
+    """
+    A base class for the Person resource to define common properties and methods.
+
+    This class defines a `get_quotes_field_type` method that will allow
+    all Resources that inherit from it to optionally include a `quotes` field
+    in the response.
+    """
+
     def __init__(self, *args, **kwargs):
         self.fields = {
             "id": fields.Integer,
             "slack_user_id": fields.String,
             "first_name": fields.String,
             "last_name": fields.String,
-            "quotes": fields.List(fields.String(attribute="content")),
         }
 
         self.parser = reqparse.RequestParser(bundle_errors=True)
 
         super().__init__(*args, **kwargs)
 
+    @property
+    def get_quotes_field_type(self):
+        return fields.List(fields.String(attribute="content"))
+
 
 class PersonResource(PersonResourceBase):
+    """
+    Implements the API methods for operating on a single Person.
+
+    The `get` method is implemented to fetch a specific
+    person by a `slack_user_id`.
+    """
 
     ERRORS = {
         "does_not_exist":
@@ -41,13 +112,20 @@ class PersonResource(PersonResourceBase):
                 404,
                 message=self.ERRORS["does_not_exist"].format(
                     slack_user_id=slack_user_id
-                ),
+                )
             )
 
         return marshal(person, self.fields), 200
 
 
 class PersonListResource(PersonResourceBase):
+    """
+    Implements the API methods for operating on multiple Persons.
+
+    The `get` method is implemented to fetch all people.
+
+    The `post` method is implemented to create a new Person.
+    """
 
     ERRORS = {
         "slack_user_id_missing": "slack_user_id is required",
@@ -101,6 +179,10 @@ class PersonListResource(PersonResourceBase):
 
 
 class QuoteResourceBase(Resource):
+    """
+    A base class for the Quote resource to define common properties and methods.
+    """
+
     def __init__(self, *args, **kwargs):
         self.fields = {
             "id": fields.Integer,
@@ -115,6 +197,12 @@ class QuoteResourceBase(Resource):
 
 
 class QuoteListResource(QuoteResourceBase):
+    """
+    Implements the API methods for operating on multiple Quotes.
+
+    The `post` method is implemented to create a new Quote.
+    """
+
     ERRORS = {
         "slack_user_id_missing": "slack_user_id is required",
         "content_missing": "content is required",
