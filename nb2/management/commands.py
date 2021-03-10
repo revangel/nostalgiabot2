731b116669e1bc5c84e3be45a1268aec5908898c
Reinier Evangelista <revangelista@securitycompass.com>
2021-03-09 20:54:43 -0800

    Add file import

--- nb2/management/commands.py
+++ nb2/management/commands.py
@@ -1,36 +1,40 @@
 import click
-from datetime import datetime
 from flask import Blueprint, current_app
 from mixer.backend.flask import mixer
 
 from nb2.models import Person, Quote
+from nb2.service.import_export import import_from_file
+
+bp = Blueprint("commands", __name__, cli_group=None)
 
 
-bp = Blueprint('commands', __name__, cli_group=None)
-
-@bp.cli.command('generate-data')
-@click.argument('people', type=click.INT)
-@click.argument('quotes', type=click.INT)
+@bp.cli.command("generate-data")
+@click.argument("people", type=click.INT)
+@click.argument("quotes", type=click.INT)
 def generate_data(people: int, quotes: int):
     """
     Create people amount of Person objects in the database each
     with quotes amount of Quotes.
     """
-    current_app.logger.info(
-        f"Generating {people} People objects with {quotes} Quotes each..."
-    )
+    current_app.logger.info(f"Generating {people} People objects with {quotes} Quotes each...")
 
     # May need to modify this when this command is unit tested
     mixer.init_app(current_app)
 
-    people = mixer.cycle(people).blend(Person,
-                                       slack_user_id=mixer.faker.isbn10,
-                                       last_name=mixer.faker.last_name)
-    quotes = mixer.cycle(quotes).blend(Quote,
-                                       person=mixer.SELECT)
+    people = mixer.cycle(people).blend(
+        Person, slack_user_id=mixer.faker.isbn10, last_name=mixer.faker.last_name
+    )
+    quotes = mixer.cycle(quotes).blend(Quote, person=mixer.SELECT)
 
     current_app.logger.info("Generated the following people:")
     for person in people:
-        current_app.logger.info(
-            f"{person.slack_user_id} - {person.first_name} {person.last_name}"
-        )
+        current_app.logger.info(f"{person.slack_user_id} - {person.first_name} {person.last_name}")
+
+
+@bp.cli.command("bulk-import")
+@click.argument("filepath", type=click.STRING)
+def bulk_import(filepath: str):
+    """
+    Bulk import NB data from a file.
+    """
+    import_from_file(filepath)
